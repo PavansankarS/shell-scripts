ec5cc02fe8a9305254df31a2082995f21d6732cf
Purnasai <132776689+purnasai199@users.noreply.github.com>
2025-01-05 16:09:42 +0530

    added 02

---
+++ 03-varables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+ PERSON1=PAVAN
+ PERSON2=SAI
+
+
+echo "$PERSON1:: are you  working"
+echo "$PERSON2:: yes i'm working working"
+echo "$PERSON1:: how are you"
+echo "$PERSON2:: im fine"
